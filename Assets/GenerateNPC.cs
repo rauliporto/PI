05ccc1733ec3dea712f4554c2419f99965f8b454
rauliporto <rauliporto@sapo.pt>
2021-11-19 00:33:53 +0000

    fdx
    Estou 1h para fazer isto

---
+++ Assets/GenerateNPC.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateNPC : MonoBehaviour
+{
+    public GameObject objectNPC;
+    public int xPos;
+    public int zPos;
+    public int npcCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        npcCounter = 0;
+        StartCoroutine(SpawnNPC());
+    }
+
+    IEnumerator SpawnNPC()
+    {
+
+        while(npcCounter <10)
+        {
+            xPos = -13;
+            zPos = -32;
+            Instantiate(objectNPC, new Vector3(xPos, 2, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            npcCounter += 1;
+        }
+       
+    }
+
+}
