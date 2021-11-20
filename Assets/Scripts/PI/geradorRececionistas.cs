d5fded2dcf79b125b14269b701d2551efdac596b
rauliporto <rauliporto@sapo.pt>
2021-11-20 00:08:52 +0000

    Update geradorRececionistas.cs

--- Assets/Scripts/PI/geradorRececionistas.cs
+++ Assets/Scripts/PI/geradorRececionistas.cs
@@ -9,10 +9,16 @@ public class geradorRececionistas : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if( numeroRecionistas > 0 )
-            Instantiate(objectRececionista, new Vector3(-24.70F, 1.55F, -8.6F), Quaternion.identity);
+        GameObject rececionista1;
+        GameObject rececionista2;
+        if (numeroRecionistas > 0)
+        {
+            rececionista1 = Instantiate(objectRececionista, new Vector3(-24.70F, 1.55F, -8.6F), Quaternion.identity);
+                    }
         if (numeroRecionistas > 1)
-            Instantiate(objectRececionista, new Vector3(-17.9F, 1.55F, -8.6F), Quaternion.identity);
+        {
+            rececionista2 = Instantiate(objectRececionista, new Vector3(-17.9F, 1.55F, -8.6F), Quaternion.identity);
+                    }
     }
 
     // Update is called once per frame
