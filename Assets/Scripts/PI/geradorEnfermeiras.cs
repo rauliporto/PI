d666892a96971d1dcb585b25247942afc88a6b2f
rauliporto <rauliporto@sapo.pt>
2021-11-20 00:08:40 +0000

    3nd

--- Assets/Scripts/PI/geradorEnfermeiras.cs
+++ Assets/Scripts/PI/geradorEnfermeiras.cs
@@ -8,14 +8,20 @@ public class geradorEnfermeiras : MonoBehaviour
     public int numeroEnfermeiras;
     // Start is called before the first frame update
     void Start()
-     
+
     {
         GameObject enfermeiro1;
         GameObject enfermeiro2;
         if (numeroEnfermeiras > 0)
-           enfermeiro1 = Instantiate(objectEnfermeiras, new Vector3(13.5F, 1.55F, 6F), Quaternion.identity);
+        {
+            enfermeiro1 = Instantiate(objectEnfermeiras, new Vector3(13.5F, 1.55F, 6F), Quaternion.identity);
+            enfermeiro1.transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
         if (numeroEnfermeiras > 1)
+        {
             enfermeiro2 = Instantiate(objectEnfermeiras, new Vector3(13.5F, 1.55F, -2F), Quaternion.identity);
+            enfermeiro2.transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
     }
 
     // Update is called once per frame
